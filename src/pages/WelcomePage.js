9cc53c6eddb75bb3128f15f2c28cbd489cd14ba6
Yahya493 <78732281+Yahya493@users.noreply.github.com>
2023-05-23 16:27:08 +0300

    Update WelcomePage.js

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -3,6 +3,6 @@ import '../styles/WelcomePage.css'
 
 export default function WelcomePage() {
   return (
-    <div>ahlan wsahlan bikom ye7ya yusef ahmad mou3tasim arjou menkom an tabda2ou bel machrou3 kabla fawet lawen </div>
+    <div>ahlan wsahlan bikom ye7ya yusef ahmad mou3tasim arjou menkom an tabda2ou bel machrou3 kabla fawet lawen</div>
   )
 }
